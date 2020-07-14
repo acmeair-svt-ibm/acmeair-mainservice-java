c5cc7dfc5f48a10acd8b6dc465f05eb728273789
Tanya-Chu <tanya.chu@ibm.com>
2020-07-14 13:06:54 -0400

    Try using xhrGet to get auth service url

--- src/main/webapp/js/acmeair-common.js
+++ src/main/webapp/js/acmeair-common.js
@@ -47,13 +47,28 @@ function updateLoggedInUserWelcome() {
 	}
 }
 
-var fr = new FileReader()
-var authUrl = fr.readAsText('/acmeair//stackaa-auth/url');
+function getAuthUrl() {
+	dojo.xhrGet({
+		url: '/acmeair/stackaa-auth/url',
+		handleAs: 'text',
+		load: function(response) {
+			return response;
+		},
+		error: function(response) {
+			console.log(response);
+		}
+	});
+}
 
 function login() {
 	hideLoginDialog();
 	showLoginWaitDialog();
-	console.log(authUrl);
+	
+	var deferred = getAuthUrl();
+	deferred.then(function(response) {
+		console.log(response);
+	})
+
 	var userString = document.getElementById('userId').value;
 	dojo.xhrPost({
 		content : {
